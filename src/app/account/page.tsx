a2ca96c60f69f01e158f4bb4f147b2be437202c4
Saim khalid <saimkhalid6220@gmail.com>
2025-11-21 17:56:46 +0500

    updated domain

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -19,7 +19,7 @@ const Account = () => {
   const actionCodeSettings = {
   // URL you want to redirect back to. The domain (www.example.com) for this
   // URL must be in the authorized domains list in the Firebase Console.
-  url: 'http://localhost:3000/welcome',
+  url: 'https://nexiler.vercel.app/welcome',
   // This must be true.
   handleCodeInApp: true,
   // The domain must be configured in Firebase Hosting and owned by the project.
