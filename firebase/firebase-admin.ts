9c99d2583aefe89b81d47f2a481f6699acc091c3
Saim khalid <saimkhalid6220@gmail.com>
2025-12-10 15:36:50 +0500

    refactored the firebase-admin code

--- firebase/firebase-admin.ts
+++ firebase/firebase-admin.ts
@@ -1,8 +1,17 @@
-import * as admin from "firebase-admin";
+import { credential, firestore } from "firebase-admin";
+import { getApp, getApps, initializeApp } from "firebase-admin/app";
 
-const serviceAccountJSON = JSON.parse(process.env.SERVICE_ACCOUNT || '{}');
-const firebaseAdmin = admin.apps.length?admin.app():admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountJSON || "")
-});
+if(!process.env.PROJECT_ID || !process.env.CLIENT_EMAIL || !process.env.PRIVATE_KEY) {
+  throw new Error("Missing Firebase admin environment variables");
+}
 
-export const admindb = firebaseAdmin.firestore();
+const adminCredentials = {
+  credential: credential.cert({
+    projectId: process.env.PROJECT_ID,
+    clientEmail: process.env.CLIENT_EMAIL,
+    privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
+};
+
+const firebaseAdminApp = getApps().length === 0 ? initializeApp(adminCredentials) : getApp();
+export const admindb = firestore(firebaseAdminApp);
